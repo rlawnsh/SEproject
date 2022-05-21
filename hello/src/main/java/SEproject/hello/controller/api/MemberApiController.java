d55d2c5a1156facf2a52cdf66887f4bcad075fce
wnshrla <wnshrla@naver.com>
2022-05-21 17:20:38 +0900

    Feat: MemberApiController

--- hello/src/main/java/SEproject/hello/controller/api/MemberApiController.java
+++ hello/src/main/java/SEproject/hello/controller/api/MemberApiController.java
@@ -66,7 +66,7 @@ public class MemberApiController {
         Member member = memberService.certificationEmail(certificationEmail.getEmail());
         LoginReq loginReq = new LoginReq(member.getMemberId(), member.getPassword());
         TokenDto token = memberService.createToken(loginReq);
-        return ResponseEntity.status(201).body(new TempToken("메일 인증에 성공했습니다.", 201, token.getAccessToken()));
+        return ResponseEntity.status(200).body(new TempToken("메일 인증에 성공했습니다.", 200, token.getAccessToken()));
     }
 
     @GetMapping("/findMemberId")
