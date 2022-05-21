acedc5f84d861096e2428b53c852806f5e3f16ef
wnshrla <wnshrla@naver.com>
2022-05-21 17:10:06 +0900

    Feat: homepage

---
+++ hello/src/main/java/SEproject/hello/controller/HomeController.java
@@ -0,0 +1,13 @@
+package SEproject.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    private String mainPage() {
+        return "home";
+    }
+}
