7a554f9ae2801279cfe9ca12d17b97fddcba723b
wnshrla <wnshrla@naver.com>
2022-05-21 17:01:08 +0900

    Feat: init commit

---
+++ hello/src/main/java/SEproject/hello/HelloApplication.java
@@ -0,0 +1,15 @@
+package SEproject.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
