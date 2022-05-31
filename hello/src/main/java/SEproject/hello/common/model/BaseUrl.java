ac6ad7c5ca0f8cf17145e9095a341e4abd200c26
wnshrla <wnshrla@naver.com>
2022-05-31 14:08:20 +0900

    Feat: test 결과 등록 url 추가(로컬)

--- hello/src/main/java/SEproject/hello/common/model/BaseUrl.java
+++ hello/src/main/java/SEproject/hello/common/model/BaseUrl.java
@@ -5,5 +5,5 @@ import javax.swing.filechooser.FileSystemView;
 public class BaseUrl {
 
     public static String test_BASE_URL = FileSystemView.getFileSystemView().getHomeDirectory().toString() + "/" + "mbtiTestImg/";
-
+    public static String testBook_Base_URL = FileSystemView.getFileSystemView().getHomeDirectory().toString() + "/" + "testBook/";
 }
