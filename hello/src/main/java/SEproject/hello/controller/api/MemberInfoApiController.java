21567a43d72c83ec6b0e17d8918d5228089a9947
wnshrla <wnshrla@naver.com>
2022-05-31 14:09:05 +0900

    Feat: Rename

--- hello/src/main/java/SEproject/hello/controller/api/MemberInfoApiController.java
+++ hello/src/main/java/SEproject/hello/controller/api/MemberInfoApiController.java
@@ -2,21 +2,25 @@ package SEproject.hello.controller.api;
 
 import SEproject.hello.common.model.BaseResponse;
 import SEproject.hello.controller.dto.MbtiTestDto;
+import SEproject.hello.controller.dto.request.PostTestReq;
 import SEproject.hello.controller.dto.response.BookmarkRes;
 import SEproject.hello.service.BookMarkService;
+import SEproject.hello.service.MbtiTestBookService;
 import SEproject.hello.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("info")
 @RequiredArgsConstructor
-public class MemberInfoController {
+public class MemberInfoApiController {
 
-    private final MemberService memberService;
+    private final MbtiTestBookService mbtiTestBookService;
     private final BookMarkService bookMarkService;
 
     @GetMapping(value = {"/bookmark/{page}", "/bookmark"})
@@ -38,6 +42,18 @@ public class MemberInfoController {
     @DeleteMapping("/delete/{mbtiTestId}")
     public void deleteBookMark(@PathVariable Long mbtiTestId) {
         bookMarkService.delete(mbtiTestId);
+    }
+
+    @PostMapping("/testThumbnail")
+    public ResponseEntity<? extends BaseResponse> uploadThumbnail(@RequestParam("images")MultipartFile multipartFile) throws IOException {
+        String fileName = mbtiTestBookService.saveThumbnail(multipartFile);
+        return ResponseEntity.status(201).body(new BaseResponse(fileName, 201));
+    }
+
+    @PostMapping("/postMbtiTest")
+    public ResponseEntity<? extends BaseResponse> postTest(@RequestBody PostTestReq postTestReq) {
+        mbtiTestBookService.saveTest(postTestReq);
+        return ResponseEntity.status(201).body(new BaseResponse("성공적으로 업로드 하였습니다.", 201));
     }
 
 }
